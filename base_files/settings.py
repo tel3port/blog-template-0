bbac8f5a5ecd656b16e044b3070e6144a3f6ad85
tel3port <carn3lian@gmail.com>
2020-09-06 01:30:46 +0300

    changed allowed_hosts to * and changed dbug to true

--- base_files/settings.py
+++ base_files/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'd+j3(=p4e!zrf#s2=1@4ojh-*-j&#mlkh6pu5l8q)8h7a+4d_4'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*"
