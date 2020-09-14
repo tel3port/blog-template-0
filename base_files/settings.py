8b0a88d5891f30d67d981039a4d8c8a4a7cee3b7
tel3port <carn3lian@gmail.com>
2020-09-14 19:36:21 +0300

    added base_files to gitignore

--- base_files/settings.py
+++ base_files/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
                  '127.0.0.1',
                  'capterra-reviews.us',
-                 'aaa.aaa',
+                 'asdsasdas.aaa',
                  'ecommerce.capterra-reviews.us',
                  'accounting-scam.capterra-reviews.us',
                  'android-scam.capterra-reviews.us',
